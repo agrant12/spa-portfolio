4025f87f7476f31a3f3f66101c709170faa7c5f9
Alvin Grant <agrant@besentient.com>
2017-01-15 09:58:47 -0500

    Write gulpfile.js for build automation, image optimization, and error checking

---
+++ gulpfile.babel.js
@@ -0,0 +1,105 @@
+'use strict';
+
+var gulp = require('gulp'),
+	sass = require('gulp-sass'),
+	autoprefixer = require('gulp-autoprefixer'),
+	uglify = require('gulp-uglify'),
+	jshint = require('gulp-jshint'),
+	cssnano = require('gulp-cssnano'),
+	babel = require('gulp-babel'),
+	concat = require('gulp-concat'),
+	imagemin = require('gulp-imagemin'),
+	cmq = require('gulp-combine-mq'),
+	rename = require('gulp-rename'),
+	add = require('gulp-add-src');
+
+const dirs = {
+	src:    './_src/',
+	build:  './public'
+},
+
+styles = {
+	src:    `${dirs.src}/sass`,
+	build:  `${dirs.build}/css`
+},
+
+scripts = {
+	src:    `${dirs.src}/scripts`,
+	build:  `${dirs.build}/js`
+},
+
+images = {
+	src:    `${dirs.src}/images`,
+	build:  `${dirs.build}/images`   
+};
+
+
+/*
+ *  CSS task
+ */
+ gulp.task('styles', function () {
+
+	console.log('starting task: [styles]');
+
+	gulp.src(`${styles.src}/app.scss`)
+	.pipe(sass({errLogToConsole: true}))
+	.pipe(autoprefixer('last 8 version'))
+	.pipe(cmq())
+	.pipe(gulp.dest(styles.build))
+	.pipe(cssnano())
+	.pipe(rename({ suffix: '.min' }))
+	.pipe(gulp.dest(styles.build));
+});
+
+
+
+/*
+ *  JS task
+ */
+ gulp.task('scripts',function(){
+
+	console.log('starting task: [scripts]');
+	
+	// Custom JS
+	gulp.src([
+		`${scripts.src}/libs/tweenmax.min.js`,
+		`${scripts.src}/app.js`
+		])
+	.pipe(babel({presets: ['es2015']}))
+	.pipe(add.prepend(`${scripts.src}/libs/tweenmax.min.js`))
+	.pipe(concat('app.js'))
+	.pipe(gulp.dest(scripts.build))
+	.pipe(concat('app.js'))
+	.pipe(uglify())
+	.pipe(rename({ suffix: '.min' }))
+	.pipe(gulp.dest(scripts.build));
+});
+
+
+/*
+ *  IMAGE minificatiooooon
+ */
+ gulp.task('images', function() {
+
+	console.log('starting task: [images]');
+
+	gulp.src(`${images.src}/*`)
+	.pipe(imagemin())
+	.pipe(gulp.dest(images.build))
+});
+
+
+/*
+ *  WATCH tasks to serve up
+ */
+ gulp.task('watch', ['styles', 'scripts'], function () {
+	gulp.watch(`${styles.src}/**/*.scss`, ['styles']);
+	gulp.watch(`${scripts.src}/**/*.js`, ['scripts']);
+});
+
+
+/*
+ *  DEFAULT tasks to serve up
+ */
+ gulp.task('default', ['styles', 'scripts']);
+
