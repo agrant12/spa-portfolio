a4fae16c08f829def2619a4b7ba654d5c8f1efe9
Alvin Grant <agrant@besentient.com>
2017-01-20 16:14:51 -0500

    Refactor navigation markup
    Create menu slide animation

--- public/js/app.js
+++ public/js/app.js
@@ -4469,25 +4469,46 @@ var app = function () {
 	};
 
 	var menuIcon = function menuIcon() {
-		var $line1 = $('.line1'),
-		    $line2 = $('.line2'),
-		    $line3 = $('.line3'),
-		    $menuIcon = $('#menu-icon'),
-		    $clicked = false;
+		var line1 = document.querySelector('.line1'),
+		    line2 = document.querySelector('.line2'),
+		    line3 = document.querySelector('.line3'),
+		    menuIcon = document.querySelectorAll('.menu-icon'),
+		    nav = document.getElementById('nav'),
+		    clicked = false;
 
-		$menuIcon.on('click', function () {
-			if (!$clicked) {
-				$line1.addClass('rotate1');
-				$line2.addClass('rotate2');
-				$line3.addClass('rotate3');
-				$clicked = true;
-			} else {
-				$line1.removeClass('rotate1');
-				$line2.removeClass('rotate2');
-				$line3.removeClass('rotate3');
-				$clicked = false;
-			}
-		});
+		for (var i = menuIcon.length - 1; i >= 0; i--) {
+			menuIcon[i].addEventListener('click', function () {
+				if (!clicked) {
+					/*line1.className = "rotate1";
+     line2.className = "rotate2";
+     line3.className = "rotate3";*/
+					nav.className += " slideRight";
+					clicked = true;
+				} else {
+					/*line1.className = "";
+     line2.className = "";
+     line3.className = "";
+     nav.className = "";*/
+					nav.className = " slideLeft";
+					clicked = false;
+				}
+			});
+		}
+		/*$menuIcon.on('click', function() {
+  	if (!$clicked) {
+  		$line1.addClass('rotate1');
+  		$line2.addClass('rotate2');
+  		$line3.addClass('rotate3');
+  		nav.style.display = 'flex';
+  		$clicked = true;
+  	} else {
+  		$line1.removeClass('rotate1');
+  		$line2.removeClass('rotate2');
+  		$line3.removeClass('rotate3');
+  		nav.style.display = 'none';
+  		$clicked = false;
+  	}
+  });*/
 	};
 
 	return {
